7bfd53d2d41a3a95eb6bdcce5f9a602c82f74484
Marcus Moody <memoody02@gmail.com>
2026-04-02 11:45:01 -0400

    Add sidebar controls for dashboard
    Added sidebar controls section with titles and comments.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,21 @@ st.title("Boston Crime Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+# --- Sidebar Controls ---
+st.sidebar.title("🔧 Dashboard Controls")
+
+    # This will be the code to split the data in pre-covid and post-covid sections
+# data_source = st.sidebar.radio(
+#     "Select Data Type:",
+#     ["Comments", "Posts"],
+#     index=0
+# )
+
+
+    # This will be the code to show data on a per district basis
+# selected_subreddit = st.sidebar.selectbox(
+#     "Filter by Subreddit:",
+#     ["All", "r/gaming", "r/news", "r/Showerthoughts"]
+# )
