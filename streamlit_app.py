14da57f2164f7ef67f0648bcf5625d49838c22ce
Marcus Moody <memoody02@gmail.com>
2026-04-05 22:38:10 -0400

    Update redlining map data source URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ def get_color(grade):
 # --- Redlining Map Function ---
 def create_redlining_map(data):
     folium.GeoJson(
-        "boston_redlining.json",
+        "https://drive.google.com/file/d/1Uk3m5_fgXTdYhlcosXhzfkcEotHm4iSM/view?usp=drive_link",
         name="Redlining Zones",
         style_function=lambda feature: {
             'fillColor': get_color(feature['properties']['grade']),
